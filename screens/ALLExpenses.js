af08b86f8143f1c6e5a7611b1828fcf62caca49a
Michael O'Grady <mogrady.professional@gmail.com>
2022-07-01 18:54:54 +0100

    Outputting static list of expenses

--- screens/ALLExpenses.js
+++ screens/ALLExpenses.js
@@ -1,6 +1,7 @@
 import { Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 function ALLExpenses() {
-  return <Text></Text>;
+  return <ExpensesOutput expensesPeriod="Total" />;
 }
 
 export default ALLExpenses;
