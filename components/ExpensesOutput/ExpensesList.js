b9079ab56c9f701b4166ca17d81c45e6d349f1cf
Michael O'Grady <mogrady.professional@gmail.com>
2022-07-01 18:34:59 +0100

    Components init for Expenses

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,7 @@
+import { FlatList } from "react-native";
+
+function ExpensesList() {
+  return <FlatList></FlatList>;
+}
+
+export default ExpensesList;
---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+function ExpensesSummary() {
+  return (
+    <View>
+      <Text>Last 7 Days</Text>
+      <Text>€100.00</Text>
+    </View>
+  );
+}
+
+export default ExpensesSummary;
